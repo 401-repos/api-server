8178aea7cb5a37790c0aa1d6fe2c672fa210183d
Omar Ramadan <ramomar1992@outlook.com>
2021-06-21 14:59:36 +0300

    fixing

--- src/routes/todo-routes.js
+++ src/routes/todo-routes.js
@@ -10,25 +10,25 @@ router.route('/todo').get(getAllHandler).post(createHandler);
 router.route('/todo/:id').get(getOneHandler).put(updateHandler).delete(deleteHandler);
 
 async function getAllHandler(req, res) {
-    const result = await todoDB.read();
-    res.status(200).json(result);
+  const result = await todoDB.read();
+  res.status(200).json(result);
 }
 async function getOneHandler(req, res) {
-    let id = req.params.id;
-    const result = await todoDB.read(id)
-    res.status(200).json(result);
+  let id = req.params.id;
+  const result = await todoDB.read(id);
+  res.status(200).json(result);
 }
 async function createHandler(req, res) {
-    const createdObj = await todoDB.add(req.body);
-    res.status(201).json(createdObj);
+  const createdObj = await todoDB.add(req.body);
+  res.status(201).json(createdObj);
 }
 async function updateHandler(req, res) {
-    const upadtedObj = await todoDB.update(req.params.id, req.body);
-    res.status(204).json(upadtedObj);
+  const upadtedObj = await todoDB.update(req.params.id, req.body);
+  res.status(204).json(upadtedObj);
 }
 async function deleteHandler(req, res) {
-    const deletedObj = await todoDB.delete(req.params.id);
-    res.status(204).json(deletedObj);
+  const deletedObj = await todoDB.delete(req.params.id);
+  res.status(204).json(deletedObj);
 }
 
 module.exports = router;
